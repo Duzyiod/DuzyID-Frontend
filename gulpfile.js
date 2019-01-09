8e12adebb501ca2c1a626eada5a075ab2b604118
Yevgeny Maslov <yevgeny.maslov@codeit.com.ua>
2019-01-09 18:42:11 +0200

    #44549: Added comment HEADER section for every source file that contains executable code

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,30 @@
+/**********************************************************************************
+ *
+ * Duzy iOD, LLC CONFIDENTIAL
+ * __________________
+ *
+ * Copyright Duzy iOD, LLC. All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains the property
+ * of Duzy iOD, LLC unless otherwise noted.
+ *
+ * The intellectual and technical concepts contained herein are proprietary
+ * to Duzy iOD, LLC and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ *
+ * Dissemination of this information or reproduction of this material is strictly
+ * forbidden unless prior written permission is obtained from Duzy iOD, LLC.
+ * This file is subject to the full terms and conditions defined
+ *
+ * ***********************************************************************************
+ * System Component: Duzy ID
+ * Module Name: Gulpfile
+ * Original Author: Andrii Sokoltsov
+ *
+ * Description: DuzyID FrontEnd code to enable Duzy products and checkout functionality over video player.
+ *
+ ************************************************************************ */
+
 'use strict';
 
 var gulp = require('gulp'),
